59d672dcdbe6c432939202c0ed9ee6c2835e7a11
Konstantin <linuksoid@gmail.com>
2018-10-09 18:51:26 +0300

    Drum class

---
+++ src/main/java/games/Drum.java
@@ -0,0 +1,16 @@
+package games;
+
+public class Drum {
+
+    final private int numOfValues;
+
+    Drum(int numOfValues) {
+        this.numOfValues = numOfValues;
+    }
+
+    public void runDrums() {
+
+
+    }
+
+}
--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -2,7 +2,34 @@ package games;
 
 public class Slot {
 
+	private int amount;
+
+	final private int bet;
+
+	final private int prize;
+
+	private int power;
+
+	final static private int NUM_OF_VALUES = 7;
+
+	private Slot(final int amount, final int bet, final int prize) {
+		this.amount = amount;
+		this.bet = bet;
+		this.prize = prize;
+	}
+
+
+
+
+
+
+
 	public static void main(String[] args) {
+		Slot slot = new Slot(100, 10, 1000);
+		Drum drum1 = new Drum(NUM_OF_VALUES);
+		Drum drum2 = new Drum(NUM_OF_VALUES);
+		Drum drum3 = new Drum(NUM_OF_VALUES);
 		System.out.println("Hello, World!");
 	}
 }
+
