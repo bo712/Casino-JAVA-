1ae9ab523586934c5cce3c13ff5f514e940ea22e
Konstantin Babakov <linuksoid@gmail.com>
2018-10-10 18:13:27 +0300

    from OOP to static

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -2,74 +2,75 @@ package games;
 
 public class Slot {
 
-	private int credit;
+    private static int[] reel = {getValueOfReel(), getValueOfReel(), getValueOfReel()}; //барабаны
 
-	private final int bet;
+    private static final int NUM_OF_VALUES = 7;
 
-	private final int prize;
+    private static int getValueOfReel() {
+        return (int)Math.round(Math.random() * NUM_OF_VALUES);
+    }
 
-	private Slot(final int credit, final int bet, final int prize) {
-		this.credit = credit;
-		this.bet = bet;
-		this.prize = prize;
-	}
+    private static int getEffort() {
+        return (int)Math.round(Math.random() * 100);
+    }
 
-	private static int getEffort() {
-		return (int)Math.round(Math.random() * 100);
-	}
+    private static void runReel(final int [] reel) {
+        for(int i = 0; i < reel.length; i += 1) {
+            reel[i] = (getEffort() + reel[i]) % NUM_OF_VALUES;
+        }
+    }
 
-	private static void printHello() {
-		System.out.println("Добро пожаловать в игру \"Однорукий бандит\"!");
-	}
+    private static void printHello() {
+        System.out.println("Добро пожаловать в игру \"Однорукий бандит\"!");
+    }
 
-	private static void printBye() {
-		System.out.println("\n\nОднорукий бандит одной рукой вынул все Ваши деньги.");
-		System.out.println("Возвращайтесь, когда найдёте ещё немного денег. Вам обязательно повезёт. Кхе-кхе =)");
-	}
+    private static void printBye() {
+        System.out.println("\n\nОднорукий бандит одной рукой вынул все Ваши деньги.");
+        System.out.println("Возвращайтесь, когда найдёте ещё немного денег. Вам обязательно повезёт. Кхе-кхе =)");
+    }
 
-	private static void printMoneyStatement(final int credit, final int bet) {
-		System.out.println("\nУ Вас $" + credit + ", ставка - $" + bet);
-		System.out.println("Крутим барабаны!Розыгрыш принёс следующие результаты:");
-	}
+    private static void printMoneyStatement(final int credit, final int bet) {
+        System.out.println("\nУ Вас $" + credit + ", ставка - $" + bet);
+        System.out.println("Крутим барабаны!Розыгрыш принёс следующие результаты:");
+    }
 
-	private static void printDrumsResult(final int firstValue, final int secondValue, final int thirdValue) {
-		System.out.println("первый барабан - " + firstValue + ", второй - " + secondValue + ", третий - " + thirdValue);
-	}
+    private static void printDrumsResult(final int firstValue, final int secondValue, final int thirdValue) {
+        System.out.println("первый барабан - " + firstValue + ", второй - " + secondValue + ", третий - " + thirdValue);
+    }
 
-	private static void printWin(final int prize, final int credit) {
-		System.out.println("\n!!!!!!!!!!!!!!!!*************************!!!!!!!!!!!!!!!!");
-		System.out.println("Вы ВЫИГРАЛИ $" + prize + "!!! Ваш капитал теперь составляет: $" + credit);
-		System.out.println("!!!!!!!!!!!!!!!!*************************!!!!!!!!!!!!!!!!");
-	}
+    private static void printWin(final int prize, final int credit) {
+        String message = "Вы ВЫИГРАЛИ $" + prize + "!!! Ваш капитал теперь составляет: $" + credit;
 
-	private static void printLose(final int bet, final int credit) {
-		System.out.println("Проигрыш $" + bet + ", ваш капитал теперь составляет: $" + credit);
-	}
+        System.out.println("\n!!!!!!!!!!!!!!!!*************************!!!!!!!!!!!!!!!!");
+        System.out.println("Вы ВЫИГРАЛИ $" + prize + "!!! Ваш капитал теперь составляет: $" + credit);
+        System.out.println("!!!!!!!!!!!!!!!!*************************!!!!!!!!!!!!!!!!");
+    }
 
-	public static void main(String[] args) {
-		Slot slot = new Slot(100, 10, 1000);
-		Reel reel1 = new Reel();
-		Reel reel2 = new Reel();
-		Reel reel3 = new Reel();
+    private static void printLose(final int bet, final int credit) {
+        System.out.println("Проигрыш $" + bet + ", ваш капитал теперь составляет: $" + credit);
+    }
 
-		printHello();
-		while (slot.credit > slot.bet) {
-			printMoneyStatement(slot.credit, slot.bet);
+    public static void main(String[] args) {
 
-			int firstValue = reel1.runReel(getEffort());
-			int secondValue = reel2.runReel(getEffort());
-			int thirdValue = reel3.runReel(getEffort());
+        int credit = 100;
+        int bet = 10;
+        int prize = 1000;
 
-			printDrumsResult(firstValue, secondValue, thirdValue);
+        printHello();
+        while (credit > bet) {
+            printMoneyStatement(credit, bet);
+            runReel(reel);
 
-			if (firstValue == secondValue && firstValue == thirdValue) {
-				slot.credit += slot.prize;
-				printWin(slot.prize, slot.credit);
-			} else {
-				slot.credit -= slot.bet;
-				printLose(slot.bet, slot.credit);
-			}
-		}
-		printBye();
-	}
+            printDrumsResult(reel[0], reel[1], reel[2]);
+
+            if (reel[0] == reel[1] && reel[0] == reel[2]) {
+                credit += prize;
+                printWin(prize, credit);
+            } else {
+                credit -= bet;
+                printLose(bet, credit);
+            }
+        }
+        printBye();
+    }
 }
