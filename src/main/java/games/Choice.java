c057adeea35746328d8b290bc8804cdaed95ff65
Konstantin Babakov <linuksoid@gmail.com>
2018-10-10 18:41:25 +0300

    make new classes for st.5

---
+++ src/main/java/games/Choice.java
@@ -0,0 +1,13 @@
+package games;
+
+public class Choice {
+
+    public static void main(String[] args) {
+        System.out.println("Выберите игру:\n1 - \"однорукий бандит\", 2 - \"пьяница\"");
+        switch (System.in.read()) {
+            case '1': Slot.main(); break;
+            case '2': Drunkard.main(); break;
+            default: System.out.println("Игры с таким номером нет!");
+        }
+    }
+}
