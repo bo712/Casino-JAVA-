d183228c7dfe84dba28fee1b8d4dd7883c1f360c
Konstantin Babakov <linuksoid@gmail.com>
2018-10-13 03:11:21 +0300

    begin talks

--- src/main/java/games/BlackJack.java
+++ src/main/java/games/BlackJack.java
@@ -1,17 +1,21 @@
 package games;
 
+import java.io.IOException;
+
 final class BlackJack {
 
 	private static final int MAX_VALUE = 21;
 	private static final int MAX_CARDS_COUNT = 8; //максимальное количество карт на руках. Иначе всегда будет перебор.
 
 	private static int[] playersMoney = {100, 100};
+	private static int bet = 10;
 
 	private static int[] cards; // Основная колода
 	private static int cursor; // Счётчик карт основной колоды
 
 	private static int[][] playersCards; // карты игроков. Первый индекс - номер игрока
 	private static int[] playersCursors; // курсоры карт игроков. Индекс - номер игрока
+	private static int[] playersPoints; // количество очков игроков. Индекс - номер игрока
 
 	private static int value(final int card) {
 		switch (CardUtils.getPar(card)) {
@@ -29,7 +33,8 @@ final class BlackJack {
 	}
 
 	private static void addCard2Player(final int player) {
-		playersCards[player][playersCursors[player]] = cards[cursor++];
+		playersCards[player][playersCursors[player]] = cards[cursor];
+		printGivenCard(player, cards[cursor++]);
 		playersCursors[player] += 1;
 	}
 
@@ -57,7 +62,46 @@ final class BlackJack {
 		playersCursors = new int[]{0, 0};
 	}
 
-	static void main() {
+	private static void playersStep(final int player) throws IOException {
+		addCard2Player(player);
+		addCard2Player(player);
+		while((player == 0 && sum(player) < 20 && confirm("Берём ещё?")) || (player == 1 && sum(player) < 17)) {
+			addCard2Player(player);
+		}
+		playersPoints[player] = getFinalSum(0);
+	}
+
+	static boolean confirm(String message) throws IOException {
+		System.out.println(message + " \"Y\" - Да, {любой другой символ} - нет (Что бы выйти из игры, нажмите Ctrl + C)");
+		switch (Choice.getCharacterFromUser()) {
+			case 'Y':
+			case 'y': return true;
+			default: return false;
+		}
+	}
+
+	static void printGivenCard(final int player, final int card) {
+		if (player == 0){
+			System.out.println("Вам выпала карта " + value(card));
+		} else {
+			System.out.println("Компьютеру выпала карта " + value(card));
+		}
+
+	}
+
+	static void main() throws IOException {
+
+		while(playersMoney[0] >= bet && playersMoney[1] >= bet){
+			initRound();
+			playersStep(0);
+			playersStep(1);
+		}
+
+		if (playersMoney[0] > 0)
+			System.out.println("Вы выиграли! Поздравляем!");
+		else
+			System.out.println("Вы проиграли. Соболезнуем...");
+
 		initRound();
 	}
 
